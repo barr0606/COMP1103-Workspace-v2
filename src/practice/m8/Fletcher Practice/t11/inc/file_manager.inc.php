26cdfc6fb2e723ba38abe2e7afe8e811cc710538
barr0606 <barr0606@flinders.edu.au>
2026-05-19 14:27:48 +0930

    fix

--- src/practice/m8/Fletcher Practice/t11/inc/file_manager.inc.php
+++ src/practice/m8/Fletcher Practice/t11/inc/file_manager.inc.php
@@ -1,11 +1,13 @@
-function get_data($filename) {
-    $path = dirname(__DIR__) . "/data/" . $filename;
+<?php
+    function get_data($filename) {
+        $path = dirname(__DIR__) . "/data/" . $filename;
 
-    if(!file_exists($path)) {
-        return[];
-    }
+        if(!file_exists($path)) {
+            return[];
+        }
 
-    $json_content = file_get_contents($path);
+        $json_content = file_get_contents($path);
 
-    return json_decode($json_content, true) ?? [];
+        return json_decode($json_content, true) ?? [];
 }
+?>
