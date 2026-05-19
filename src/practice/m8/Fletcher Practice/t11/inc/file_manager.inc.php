a779b6d9451711afeccd6580d3af80a20aad2a34
barr0606 <barr0606@flinders.edu.au>
2026-05-19 14:30:20 +0930

    Finally fixed

--- src/practice/m8/Fletcher Practice/t11/inc/file_manager.inc.php
+++ src/practice/m8/Fletcher Practice/t11/inc/file_manager.inc.php
@@ -1,6 +1,6 @@
 <?php
     function get_data($filename) {
-        $path = dirname(__DIR__) . "/data/" . $filename;
+        $path = __DIR__ . "/data/" . $filename;
 
         if(!file_exists($path)) {
             return[];
