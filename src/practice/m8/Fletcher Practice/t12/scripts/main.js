222a83240df6adecea5ccfd8a12ff762fd7bdab3
barr0606 <barr0606@flinders.edu.au>
2026-05-20 13:11:36 +0930

    task 13

--- src/practice/m8/Fletcher Practice/t12/scripts/main.js
+++ src/practice/m8/Fletcher Practice/t12/scripts/main.js
@@ -27,6 +27,19 @@ function handleFilter() {
 }
 
 
+function showView(viewId) {
+    if (viewId === 'animal-view') {
+        animalView.style.display = 'block';
+        appview.style.display = 'none';
+    }
+
+    if (viewId === 'app-view') {
+        animalView.style.display = 'none';
+        appview.style.display = 'block';
+    }
+}
+
+
 function renderAnimals(animalList) {
     if (animalContainer) {;
         
