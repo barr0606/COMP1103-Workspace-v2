8ee862d5a23383856d607802d8b4fdcca65b78bc
barr0606 <barr0606@flinders.edu.au>
2026-05-20 17:43:53 +0930

    task 15 setup

---
+++ src/practice/m8/Fletcher Practice/t15/inc/footer.php
@@ -0,0 +1,11 @@
+<body>
+    <footer>
+        <p>
+            © 2026 Happy Paws Shelter
+        </p>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <p><i class="fa-brands fa-facebook"></i> <i class="fa-brands fa-instagram"></i> <i class="fa-brands fa-x-twitter"></i></p>
+    </footer>
+</body>
+
+</html>
---
+++ src/practice/m8/Fletcher Practice/t15/index.php
@@ -0,0 +1,24 @@
+<?php include 'inc/header.php'; ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta author="Fletcher Barry" description="Happy Paws Animal Shelter Homepage">
+    <title>Happy Paws - Home</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+
+<body>
+    <main>
+        <h2>
+            Welcome to Our Shelter
+        </h2>
+        <p>
+            Welcome to Happy Paws! Our mission is to connect homless pets with loving families. We believe every animal
+            deserves a safe home and a second chance. Broswe our available animals or submit an application today to
+            start your adoption journey.
+        </p>
+    </main>
+</body>
+
+<?php include 'inc/footer.php'; ?>
