82e0f447e1fdd79238270f7add8e1b17be24879b
barr0606 <barr0606@flinders.edu.au>
2026-05-22 11:03:20 +0930

    Fixed files

--- src/a2/website solution/inc/header.php
+++ src/a2/website solution/inc/header.php
@@ -9,7 +9,7 @@
                 <li><a href="Home.php">Home Page</a></li>
                 <li><a href="AboutUs.php">About Us</a></li>
                 <li><a href="SearchResults.php">Search</a></li>
-                <li><a href="Application.php">Apply to Adopt</a></li>
+                <li><a href="Applications.php">Apply to Adopt</a></li>
                 <li><a href="Blog.php">Blog</a></li>
             </ul>
         </nav>
