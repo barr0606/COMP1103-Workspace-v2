78831eab494db306312179d04bded976061925a4
munr0117 <munr0117@flinders.edu.au>
2026-05-22 14:33:31 +0930

    updates to admin

--- src/a2/website solution/admin.php
+++ src/a2/website solution/admin.php
@@ -1,3 +1,7 @@
+<?php
+include 'inc/header.php';
+?>
+
 <form action="users.php" method="POST" onsubmit="return validateLogin()">
     <input type="text" name="username" id="username">
     <input type="password" name="password" id="password">
@@ -5,3 +9,8 @@
 </form>
 
 <script src="users.js"></script>
+
+
+<?php
+include 'inc/footer.php';
+?>
