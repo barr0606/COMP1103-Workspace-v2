acd079a4a28e8e68f1790f97cec53da334397f50
munr0117 <munr0117@flinders.edu.au>
2026-05-22 14:36:12 +0930

    updates to admin

--- src/a2/website solution/admin.php
+++ src/a2/website solution/admin.php
@@ -2,6 +2,16 @@
 include 'inc/header.php';
 ?>
 
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Oliver Munro" description="Admin Page for [Website Name]">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+
 <form action="users.php" method="POST" onsubmit="return validateLogin()">
     <input type="text" name="username" id="username">
     <input type="password" name="password" id="password">
