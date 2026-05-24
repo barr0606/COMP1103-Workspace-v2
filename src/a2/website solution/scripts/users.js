775799a16ce3b7474cf03ba76cde59d322576fce
munr0117 <munr0117@flinders.edu.au>
2026-05-24 21:56:44 +0930

    changes

--- src/a2/website solution/scripts/users.js
+++ src/a2/website solution/scripts/users.js
@@ -1,10 +1,15 @@
+console.log("users.js loaded");
+
+
+const u = document.getElementById("username").value.trim();
+const p = document.getElementById("password").value.trim();
+
 function validateLogin() {
-    const u = document.getElementById("username").value.trim();
-    const p = document.getElementById("password").value.trim();
 
     if (u === "" || p === "") {
         alert("Please fill in all fields");
         return false;
     }
     return true;
+
 }
