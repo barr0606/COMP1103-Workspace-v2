4643301f109561c52d24526cc69ba4adcd4322b4
munr0117 <munr0117@flinders.edu.au>
2026-05-25 03:13:57 +0930

    changes, bugfixes, etc

---
+++ src/a2/website solution/inc/deleteApplicationAdminMain.php
@@ -0,0 +1,14 @@
+<?php
+$file = __DIR__ . "/../data/applications.json";
+
+$apps = json_decode(file_get_contents($file), true);
+
+$index = $_POST['index'];
+
+// Remove the entry
+array_splice($apps, $index, 1);
+
+file_put_contents($file, json_encode($apps, JSON_PRETTY_PRINT));
+
+header("Location: ../AdminMain.php");
+exit();
---
+++ src/a2/website solution/inc/markReviewedAdminMain.php
@@ -0,0 +1,14 @@
+<?php
+$file = __DIR__ . "/../data/applications.json";
+
+$apps = json_decode(file_get_contents($file), true);
+
+$index = $_POST['index'];
+
+// Add reviewed flag
+$apps[$index]['reviewed'] = true;
+
+file_put_contents($file, json_encode($apps, JSON_PRETTY_PRINT));
+
+header("Location: ../AdminMain.php");
+exit();
